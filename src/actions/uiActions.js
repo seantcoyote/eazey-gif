e36374f7204c4fc9fd2d17ef81cbddb050f3c889
seantcoyote <seantcoyote@gmail.com>
2018-02-02 09:31:59 -0800

    Sets up UI actions

--- src/actions/uiActions.js
+++ src/actions/uiActions.js
@@ -1,6 +1,16 @@
-import * as types from './actionTypes.js'
+import * as types from './actionTypes'
 
-export const updateNumberOfAjaxCalls = (numberOfChange) => ({
-  type: types.UPDATE_NUMBER_OF_AJAX_CALLS,
-  payload: numberOfChange
+export const updateSearchQuery = (query) => ({
+  type: types.UPDATE_SEACH_QUERY,
+  payload: query
+});
+
+export const setLoadError = (isError) => ({
+  type: types.SET_LOAD_ERROR,
+  payload: isError
+})
+
+export const setNumberOfAjaxCalls = (change) => ({
+  type: types.SET_NUMBER_OF_AJAX_CALLS,
+  payload: change
 })
--- src/actions/uiActions.test.js
+++ src/actions/uiActions.test.js
@@ -2,10 +2,22 @@ import * as types from './actionTypes'
 import * as actions from './uiActions'
 
 describe('uiActions', () => {
-  test('updateNumberOfAjaxCalls creates an UPDATE_NUMBER_OF_AJAX_CALLS action', () => {
-    expect(actions.updateNumberOfAjaxCalls(true)).toEqual({
-      type: types.UPDATE_NUMBER_OF_AJAX_CALLS,
+  test('updateSearchQuery creates an UPDATE_SEACH_QUERY action', () => {
+    expect(actions.updateSearchQuery('eazey goes it')).toEqual({
+      type: types.UPDATE_SEACH_QUERY,
+      payload: 'eazey goes it'
+    })
+  })
+  test('setLoadError creates a SET_LOAD_ERROR action', () => {
+    expect(actions.setLoadError(true)).toEqual({
+      type: types.SET_LOAD_ERROR,
       payload: true
+    })
+  })
+  test('updateNumberOfAjaxCalls creates an UPDATE_NUMBER_OF_AJAX_CALLS action', () => {
+    expect(actions.updateNumberOfAjaxCalls(1)).toEqual({
+      type: types.UPDATE_NUMBER_OF_AJAX_CALLS,
+      payload: 1
     })
   })
 })
